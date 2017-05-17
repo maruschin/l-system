ae7be63061cbbea304ab6d539d8cd87d4e0b69cd
Maruschin Eugene <maruschin@gmail.com>
2017-05-17 18:28:34 +1000

    some changes

--- koch_islands.py
+++ koch_islands.py
@@ -11,57 +11,70 @@ white = (255, 255, 255, 255)
 size  = (1000, 1000)
 
 iterations = 4
-angel = pi/3
-start_point = (450, 450)
-width_point = 1
+angel = pi/180*22.5
+start_point = (500, 1000)
+width_point = 2
 step_length = 1
-rule = 'L'
+axiom = 'F'
 producitons = {
-    'L': 'L+R++R-L--LL-R+',
-    'R': '-L+RR++R+L--L-R'
+    'F': 'F[FF+F][F]+F-F'
 }
 
-def rule_production(rule, producitons, iterations):
+def rule_production(axiom, producitons, iterations):
     producitons['+'] = '+'
     producitons['-'] = '-'
+    producitons['['] = '['
+    producitons[']'] = ']'
+    rule = axiom
     for i in range(iterations):
         rule = ''.join([producitons[s] for s in rule])
     return rule
 
+def point_add(point1, point2):
+    return point1[0] + point2[0], point1[1] + point2[1]
+
+def rad_to_euc(r, phi):
+    x = r * sin(phi)
+    y = r * cos(phi)
+    return x, y
+
 def draw_koch_islands(angel, rule, start_point, width_point, step_length):
     canvas = Image.new('RGBA', size, black)
     draw = ImageDraw.Draw(canvas)
-    curr_angel = angel
+    curr_angel = pi
+    save_angel = []
     for r in rule:
-        if r in 'FLR':
-            end_point = (start_point[0] - step_length*cos(curr_angel),
-                         start_point[1] - step_length*sin(curr_angel))
+        if r == 'F':
+            end_point = point_add(start_point, rad_to_euc(step_length, curr_angel))
             draw.line([start_point, end_point], fill=white, width=width_point)
             start_point = end_point
         elif r == 'f':
-            end_point = (start_point[0] - step_length*cos(curr_angel),
-                         start_point[1] - step_length*sin(curr_angel))
+            end_point = point_add(start_point, rad_to_euc(step_length, curr_angel))
             start_point = end_point
         elif r == '-':
             curr_angel -= angel
         elif r == '+':
             curr_angel += angel
+        elif r == '[':
+            save_angel.append((curr_angel, start_point))
+        elif r == ']':
+            curr_angel, start_point = save_angel.pop()
         else:
             pass
     return canvas
 
-rule = rule_production(rule, producitons, iterations)
+rule = rule_production(axiom, producitons, iterations)
 
-def make_dimensions(size=(600, 600)):
+def make_dimensions(rule, size=(600, 600)):
     '''Функция определяет необходимую длину шага для того, чтобы наша фигура
     поместилась на полотно заданного размера'''
     start_point = (0, 0)
     x_dim, y_dim = (0, 0), (0, 0)
-    curr_angel = angel
+    curr_angel = pi
+    save_angel = []
     for r in rule:
-        if r in 'fFLR':
-            end_point = (start_point[0] - cos(curr_angel),
-                         start_point[1] - sin(curr_angel))
+        if r in 'fF':
+            end_point = point_add(start_point, rad_to_euc(step_length, curr_angel))
             start_point = end_point
             x_dim = min(x_dim[0], end_point[0]), max(x_dim[1], end_point[0])
             y_dim = min(y_dim[0], end_point[1]), max(y_dim[1], end_point[1])
@@ -69,6 +82,12 @@ def make_dimensions(size=(600, 600)):
             curr_angel -= angel
         elif r == '+':
             curr_angel += angel
+        elif r == '[':
+            save_angel.append((curr_angel, start_point))
+        elif r == ']':
+            curr_angel, start_point = save_angel.pop()
+        else:
+            pass
     
     x_width = sum([abs(x) for x in x_dim])
     y_width = sum([abs(y) for y in y_dim])
@@ -80,8 +99,8 @@ def make_dimensions(size=(600, 600)):
     
     return center, step
             
-start_point, step_length = make_dimensions(size)
+start_point, step_length = make_dimensions(rule, size)
 canvas = draw_koch_islands(angel, rule, start_point, width_point, step_length)
 
-#canvas = draw_koch_islands(angel, rule, (300, 300),  3, 5)
+#canvas = draw_koch_islands(angel, rule, start_point,  3, 5)
 canvas.show()
