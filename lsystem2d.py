e932fb1f435ac40fdc1d533868642265224f94c0
Maruschin Eugene <maruschin@gmail.com>
2017-05-21 14:50:29 +1000

    Fix error with angel

--- lsystem2d.py
+++ lsystem2d.py
@@ -46,14 +46,14 @@ def rad_to_euc(r, phi):
 
 def draw_koch_islands(angel, rule, start_point, width_point, step_length):
     '''Draw figure'''
-    canvas = Image.new('RGBA', size, black)
+    canvas = Image.new('RGBA', size, white)
     draw = ImageDraw.Draw(canvas)
     xy_angel = np.pi
     save_angel = []
     for r in rule:
         if r == 'F':
             end_point = start_point + np.array(rad_to_euc(step_length, xy_angel))
-            draw.line([tuple(start_point), tuple(end_point)], fill=white, width=width_point)
+            draw.line([tuple(start_point), tuple(end_point)], fill=black, width=width_point)
             start_point = end_point
         elif r == 'f':
             start_point += np.array(rad_to_euc(step_length, curr_angel))
@@ -69,7 +69,7 @@ def draw_koch_islands(angel, rule, start_point, width_point, step_length):
             pass
     return canvas
 
-def make_dimensions(rule, size):
+def make_dimensions(angel, rule, size):
     '''Count step length and start point to fit in canvas size'''
     x_dim = y_dim = np.array((0, 0))
     xy_point = np.array((0,0), dtype='float64')
@@ -105,7 +105,7 @@ def deg_to_rad(deg):
     return np.pi/180*deg
 
 # Count step length and start point to fit in canvas size
-start_point, step_length = make_dimensions(rule, size)
+start_point, step_length = make_dimensions(deg_to_rad(angel), rule, size)
 
 # Draw image
 canvas = draw_koch_islands(deg_to_rad(angel), rule, start_point, width_point, step_length)
