1bd841e2fd1e83be7e739bd5c4e7a6862f2c6ef3
Evgenii Marushchenko <maruschenko.ea@dns-shop.ru>
2018-09-28 15:14:46 +1000

    Let's make a release 0.1.0

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lsystem',
+    version='0.1.0',
+    description='The funniest joke in the world',
+    url='http://github.com/maruschin/l-system',
+    author='Evgenii Marushchenko (Eugene Maruschenko)',
+    author_email='maruschin@gmail.com',
+    license='MIT',
+    packages=['lsystem'],
+    zip_safe=False,
+)
